52c873c7866c6a2a4679d16e240605da8fc73a33
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-07-18 18:22:04 -0500

    generated auth, reviews and my movies components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MoviesComponent } from './movies/movies.component';
+import { MyMoviesComponent } from './movies/my-movies/my-movies.component';
+import { AuthComponent } from './auth/auth.component';
+import { ReviewsComponent } from './movies/reviews/reviews.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MyMoviesComponent,
+    AuthComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
